1227729b9db5920508387ba1b1806dbeacbbdada
wantao666 <895484122@qq.com>
2020-09-06 22:21:23 +0800

    register

--- src/db/sync.js
+++ src/db/sync.js
@@ -1,9 +1,10 @@
-const sequelize = require('../db/seq')
-
-sequelize.async({
+const sequelize = require('./seq')
+require('./model/index')
+sequelize.sync({
     force: true  //如果表已经存在，则删除表后再创建
 }).then((result) => {
     console.log('db async ok')
+    process.exit()
 }).catch((err) => {
     console.log('db async error')
     console.log(err)
