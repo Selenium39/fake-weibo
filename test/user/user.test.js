fb4617d32f65fc86d776ca1c82a85e0a9cd3c6c1
wantao666 <895484122@qq.com>
2020-09-13 15:46:46 +0800

    setting test

--- test/user/user.test.js
+++ test/user/user.test.js
@@ -59,11 +59,42 @@ test('login', async () => {
     COOKIE = res.headers['set-cookie'].join(';')
 })
 
+//修改基本信息
+test('update info', async () => {
+    const res = await server.patch('/api/user/changeInfo').send({
+        nickeName: '测试昵称',
+        city: '测试城市',
+        picture: '/test.png'
+    }).set('cookie', COOKIE)
+    expect(res.body.errCode).toBe(0)
+})
+
+// 修改密码
+test('修改密码应该成功', async () => {
+    const res = await server
+      .patch('/api/user/changePassword')
+      .send({
+        password,
+        newPassword: `P_${Date.now()}`
+      })
+      .set('cookie', COOKIE) // 传入cookie 是为了绕过登录验证 证明已登录
+    expect(res.body.errCode).toBe(0)
+  })
+  
 //删除
 test('delete user', async () => {
     const res = await server.post('/api/user/delete').set('cookie', COOKIE) //设置cookie，绕过登录
     expect(res.body.errCode).toBe(0)
 })
+
+  
+  // 退出
+  test('退出登录应该成功', async () => {
+    const res = await server 
+      .post('/api/user/logout') 
+      .set('cookie', COOKIE)
+    expect(res.body.errCode).toBe(0)
+  })
 
 //再次查询用户，应该不存在
 test('test username is exsit again', async () => {
