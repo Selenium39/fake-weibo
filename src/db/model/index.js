cc4bfd3c1f9f5ec4283e2bbbb7394c13c8576353
wantao666 <895484122@qq.com>
2020-09-13 19:26:50 +0800

    blog model

--- src/db/model/index.js
+++ src/db/model/index.js
@@ -3,6 +3,17 @@
  */
 
 const User = require('./User')
+const Blog = require('./Blog')
+
+//User一对多Blog Blog-->User
+Blog.belongsTo(User, {
+    foreignKey: 'userId'
+})
+
+// User->Blog
+// User.hasMany(Blog) 
+
 module.exports = {
-    User
+    User,
+    Blog
 }
