52838d642d16aaf55ca91c879c314643f948c174
wantao666 <895484122@qq.com>
2020-09-13 22:38:39 +0800

    profile

---
+++ src/controller/profile.js
@@ -0,0 +1,36 @@
+/**
+ * 个人主页
+ */
+
+const {
+    getBlogListByUser
+} = require("../service/blog")
+const {
+    DEFAULT_PAGE_SIZE
+} = require('../conf/constant')
+
+const {
+    SuccessResult
+} = require('../model/ErrorInfo')
+
+async function getProfileBlogList(userName, pageIndex = 0) {
+    const result = await getBlogListByUser({
+        userName,
+        pageIndex,
+        pageSize: DEFAULT_PAGE_SIZE
+    })
+    const blogList = result.blogList
+
+    //拼接返回数据
+    return new SuccessResult({
+        isEmpty: blogList.length === 0,
+        blogList,
+        pageSize: DEFAULT_PAGE_SIZE,
+        pageIndex,
+        count: result.count
+    })
+}
+
+module.exports = {
+    getProfileBlogList
+}
--- src/service/blog.js
+++ src/service/blog.js
@@ -1,5 +1,6 @@
 const {
-    Blog
+    Blog,
+    User
 } = require("../db/model/index")
 
 async function createBlog({
@@ -15,6 +16,48 @@ async function createBlog({
     return result.dataValues
 }
 
+const {
+    formatUser
+} = require('./format')
+
+
+async function getBlogListByUser({
+    userName,
+    pageIndex = 0,
+    pageSize = 10
+}) {
+    //拼接查询条件
+    const userWhereOpts = {}
+    if (userName) {
+        userWhereOpts.userName = userName
+    }
+
+    //执行查询
+    const result = await Blog.findAndCountAll({
+        limit: pageSize,
+        offset: pageSize * pageIndex,
+        order: [
+            ['id', 'desc']
+        ],
+        include: [{ //Blog-->User 联表查询
+            model: User,
+            attributes: ['userName', 'nickName', 'picture'],
+            where: userWhereOpts
+        }]
+    })
+    //result.count 总数
+    //result.rows 查询结果 []
+
+    let blogList = result.rows.map(row => row.dataValues) //获取所有Blog数据
+    blogList = blogList.map(blogItem => { //获取每条Blog对应的User数据
+        let user = blogItem.user.dataValues
+        blogItem.user = formatUser(user)
+    })
+    return blogList
+
+}
+
 module.exports = {
-    createBlog
+    createBlog,
+    getBlogListByUser
 }
